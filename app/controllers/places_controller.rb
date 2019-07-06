8984fd7606524931db9f1f0e871180091036b060
simonayele <simonayele@gmail.com>
2019-07-06 17:25:09 +0000

    test pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.order("name").page(params[:page]).per_page()
+    @places = Place.order("name").page(params[:page]).per_page(1)
   end
 end
