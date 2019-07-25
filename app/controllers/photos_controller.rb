d0c4c38c2a34a8e2dfd3e5fa7bb03e98038664d4
simonayele <simonayele@gmail.com>
2019-07-25 20:52:04 +0000

    adding photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])  
     @place.photos.create(photo_params.merge(user: current_user))
-    redirect_to place_photos_path(@place)
+    redirect_to place_path(@place)
   end
 end
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
-class Photo < ActiveRecord::Base
+class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
 end
