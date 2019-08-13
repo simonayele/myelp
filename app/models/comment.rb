16142215434b8379648d29b83a3ad5a7dd7cf93d
simonayele <simonayele@gmail.com>
2019-08-13 16:07:50 +0000

    sending email when comments are added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
-
+  after_create :send_comment_email
+  
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_stars',
@@ -10,8 +11,12 @@ class Comment < ApplicationRecord
     'five stars': '5_stars'
   }
 
-   def humanized_rating
+  def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver_now
   end
   
 end
